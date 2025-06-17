071bcf37441b88d620a69a343e2369fc9dfb33ab
joonlee <96dalee@gmail.com>
2025-06-17 11:04:17 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+
+        try {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch(ArithmeticException e) {
+            System.out.println(e.getMessage());
+
+            System.out.println("Error: Division by zero.");
+        }
+    }
+}
